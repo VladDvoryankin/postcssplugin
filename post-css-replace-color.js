7c0f95936eabe8e96d2b07b42bf13ff08c99879a
Vlad Dvoryankin <vlad.dvoryankin@graph.uk>
2017-09-09 04:25:57 +0300

    parsing values with css-tree

--- post-css-replace-color.js
+++ post-css-replace-color.js
@@ -1,83 +1,52 @@
 const postcss = require('postcss');
 const parseColor = require('parse-color');
+const csstree = require('css-tree');
 
 function checkIfTargetProp(prop, targetProps) {
 	targetProps = targetProps || [];
 	return targetProps.indexOf(prop) > -1;
 }
 
-function compareRegExpMatchToColorArr(regExpMatch, colorArr) {
-	if (!regExpMatch || !colorArr) {
-		return false;
-	}
-
-	for(let i = 0; i < colorArr.length; i++) {
-		if (parseInt(regExpMatch[i + 1]) !== parseInt(colorArr[i])) {
-			return false;
-		}
-	}
-
-	return true;
-}
-
 function makeShortHex(hex) {
-	return '#' + hex.substring(1, 2) + hex.substring(3, 4) + hex.substring(5, 6);
+	return hex.substring(0, 1) + hex.substring(2, 3) + hex.substring(4, 5);
 }
 
-function compareShortHexToFullHex(short, full) {
-	return short && full && short[0].toLowerCase() === makeShortHex(full);
+function isColorParamsEqual(ast, color) {
+	if (!color) {
+		return false; //transparent has undefined color params
+	}
+
+	let equal = true;
+	let i = -1;
+
+	csstree.walk(ast, item => {
+		if (item.type === 'Number' || item.type === 'Percentage') {
+			i += 1;
+			if (i < 3 && parseInt(item.value) !== color[i]) {
+				equal = equal && false;
+			}
+		}
+	});
+
+	return equal;
 }
 
-function replaceColorIfFound(string, color, newColor) {
-	// word
-	const wordReg = new RegExp('(^' + color.keyword + '$)|(\s' + color.keyword + ')|(' + color.keyword + '\s)', 'i');
-	if (wordReg.test(string)) {
-		return string.replace(wordReg, (newColor.keyword || newColor.hex));
-	}
+function changeColorParams(ast, color) {
+	let i = -1;
 
-	// 3 hex
-	const hex3Reg = new RegExp(/((#[0-9a-zA-Z]{3}$)|(#[0-9a-zA-Z]{3}\s))/, 'i');
-	const hex3Values = string.match(hex3Reg);
-	if (compareShortHexToFullHex(hex3Values, color.hex)) {
-		return string.replace(new RegExp(/#[0-9a-zA-Z]{3}$/i), makeShortHex(newColor.hex));
-	}
+	csstree.walk(ast, item => {
+		if (item.type === 'Number' || item.type === 'Percentage') {
+			i += 1;
+			if (i < 3 && item.value !== color[i]) {
+				item.value = color[i];
+			}
+		}
+	});
+}
 
-	// 6 hex
-	const hex6Reg = new RegExp(/(#[0-9a-zA-Z]{6})/, 'gi');
-	const hex6Values = string.match(hex6Reg);
-	if (hex6Reg.test(string) && hex6Values[0].toLowerCase() === color.hex) {
-		return string.replace(hex6Reg, newColor.hex);
-	}
-
-	// rgb
-	const rgbReg = new RegExp(/rgb\(([0-9]+),\s?([0-9]+),\s?([0-9]+)\)/, 'i');
-	const rgbValues = string.match(rgbReg);
-	if (compareRegExpMatchToColorArr(rgbValues, color.rgb)) {
-		return string.replace(rgbReg, 'rgb(' + newColor.rgb[0] + ', ' + newColor.rgb[1] + ', ' + newColor.rgb[2] + ')');
-	}
-
-	// rgba
-	const rgbaReg = new RegExp(/rgba\(([0-9]+),\s?([0-9]+),\s?([0-9]+),\s?([\.0-9]+)\)/, 'i');
-	const rgbaValues = string.match(rgbaReg);
-	if (compareRegExpMatchToColorArr(rgbaValues, color.rgb)) {
-		return string.replace(rgbaReg, 'rgba(' + newColor.rgba[0] + ', ' + newColor.rgba[1] + ', ' + newColor.rgba[2] + ', ' + rgbaValues[4] + ')');
-	}
-
-	// hsl
-	const hslReg = new RegExp(/hsl\(([0-9]+),\s?([0-9]+)\%,\s?([0-9]+)\%\)/, 'i');
-	const hslValues = string.match(hslReg);
-	if (compareRegExpMatchToColorArr(hslValues, color.hsl)) {
-		return string.replace(hslReg, 'hsl(' + newColor.hsl[0] + ', ' + newColor.hsl[1] + '%, ' + newColor.hsl[2] + '%)');
-	}
-
-	// hsla
-	const hslaReg = new RegExp(/hsla\(([0-9]+),\s?([0-9]+)\%,\s?([0-9]+)\%,\s?([\.0-9]+)\)/, 'i');
-	const hslaValues = string.match(hslaReg);
-	if (compareRegExpMatchToColorArr(hslaValues, color.hsl)) {
-		return string.replace(hslaReg, 'hsla(' + newColor.hsla[0] + ', ' + newColor.hsla[1] + '%, ' + newColor.hsla[2] + '%, ' + hslaValues[4] + ')');
-	}
-
-	return string;
+function changeToTransparent(node) {
+	node.type = 'Identifier';
+	node.name = 'transparent';
 }
 
 module.exports = postcss.plugin('replace-color', opts => {
@@ -86,15 +55,61 @@ module.exports = postcss.plugin('replace-color', opts => {
 		to: parseColor((opts.to ? opts.to : 'white'))
 	});
 
-	console.log('\n\nReplace color:\n\n', opts.from, '\n\nto:\n\n', opts.to, '\n')
+	opts.from.keyword = (opts.from.keyword || '').toLowerCase();
+	opts.to.keyword = (opts.to.keyword || '').toLowerCase();
+	if (opts.from.hex) {
+		opts.from.hex = opts.from.hex.replace('#', '');
+	}
+	if (opts.to.hex) {
+		opts.to.hex = opts.to.hex.replace('#', '');
+	}
 
-    return root => {
-        root.walkRules(rule => {
-            rule.walkDecls(decl => {
-                if (checkIfTargetProp(decl.prop, opts.targetProps)) {
-                	decl.value = replaceColorIfFound(decl.value, opts.from, opts.to);
-                }
-            });
-        });
-    };
+	console.log('\n\nReplace:\n', JSON.stringify(opts.from), '\n\nWith:\n', JSON.stringify(opts.to), '\n')
+
+	return root => {
+		root.walkRules(rule => {
+			rule.walkDecls(decl => {
+				if (checkIfTargetProp(decl.prop, opts.targetProps)) {
+					const parsedValue = csstree.parse(decl.value, { context: 'value' });
+
+					csstree.walk(parsedValue, node => {
+						if (node.type === 'Identifier' && node.name === opts.from.keyword) {
+							console.log('\n', decl.value, '|', node.type, ' > ', opts.to.keyword)
+							node.name = opts.to.keyword;
+						} else if (opts.from.hex && node.type === 'HexColor' && node.value.length === 3 && node.value === makeShortHex(opts.from.hex)) {
+							if (opts.to.keyword === 'transparent') {
+								console.log('\n', decl.value, '|', node.type, ' > ', opts.to.keyword)
+								changeToTransparent(node);
+							} else {
+								console.log('\n', decl.value, '|', node.type, ' > ', makeShortHex(opts.to.hex))
+								node.value = makeShortHex(opts.to.hex);
+							}
+						} else if (opts.from.hex && node.type === 'HexColor' && node.value.length === 6 && node.value === opts.from.hex) {
+							if (opts.to.keyword === 'transparent') {
+								console.log('\n', decl.value, '|', node.type, ' > ', opts.to.keyword)
+								changeToTransparent(node);
+							} else {
+								console.log('\n', decl.value, '|', node.type, ' > ', opts.to.hex)
+								node.value = opts.to.hex;
+							}
+						} else if (node.type === 'Function' && (node.name === 'rgb' || node.name === 'rgba' || node.name === 'hsl' || node.name === 'hsla')) {
+							if (isColorParamsEqual(node, opts.from[node.name])) {
+								if (opts.to.keyword === 'transparent') {
+									console.log('\n', decl.value, '|', node.type, ' > ', opts.to.keyword)
+									changeToTransparent(node);
+								} else {
+									console.log('\n', decl.value, '|', node.type, ' > ', opts.to[node.name][0], opts.to[node.name][1], opts.to[node.name][2])
+									changeColorParams(node, opts.to[node.name])
+								}
+							}
+						}
+					});
+
+					decl.value = csstree.translate(parsedValue);
+				}
+			});
+		});
+
+		console.log('\n')
+	};
 });
--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,5 @@ const config = require('./post-css-replace-color-config.json');
 module.exports = {
   plugins: [
     require('./post-css-replace-color.js')(config)
-  ]
+  ],
 };
